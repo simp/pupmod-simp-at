1eb538a5e726a9f5cd1c8ecc0c434ee95846e2d3
Nick Miller <nick.miller@onyxpoint.com>
2016-12-20 09:32:46 -0500

    (SIMP-2225) Create at module (#1)
    * (SIMP-2225) Create at module
    
    This material used to reside in simplib.
    
    First cut.
    
    SIMP-2225 #close
    SIMP-1679 #comment Created at module
    
    * Moved version to 0.0.1
    
    * Updated nodesets
    
    * Updated .puppet-lint.rc
    
    * Fixed version reqs in metadata.json

---
+++ manifests/user.pp
@@ -0,0 +1,15 @@
+# Add the user $name to /etc/at.allow
+#
+# @param name
+#   The user to add to /etc/at.allow
+#
+define at::user {
+  include '::at'
+
+  $_name = regsubst($name,'/','__')
+
+  simpcat_fragment { "at+${_name}.user":
+    content =>  "#${name}\n"
+  }
+
+}
---
+++ spec/classes/init_spec.rb
@@ -0,0 +1,37 @@
+require 'spec_helper'
+
+describe 'at' do
+  context 'supported operating systems' do
+    on_supported_os.each do |os, facts|
+      context "on #{os}" do
+        let(:facts) do
+          facts
+        end
+
+        context 'with default parameters' do
+          it { is_expected.to compile.with_all_deps }
+          it { is_expected.to create_class('at') }
+          it { is_expected.to create_at__user('root') }
+          it { is_expected.to create_simpcat_build('at') }
+          it { is_expected.to create_file('/etc/at.deny').with({:ensure => 'absent'}) }
+          it { is_expected.to create_service('atd').with({
+            :ensure     => 'running',
+            :enable     => true,
+            :hasstatus  => true,
+            :hasrestart => true
+          }) }
+        end
+
+        context 'with a users parameter' do
+          let(:params) {{
+            :users => ['test','foo','bar']
+          }}
+          it { is_expected.to create_at__user('test') }
+          it { is_expected.to create_at__user('foo') }
+          it { is_expected.to create_at__user('bar') }
+        end
+
+      end
+    end
+  end
+end
