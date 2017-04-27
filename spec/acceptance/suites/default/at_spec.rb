e8ec428af492f863004314845a83fe247212e3a8
Trevor Vaughan <tvaughan@onyxpoint.com>
2017-04-26 22:14:24 -0400

    (SIMP-3111) Don't comment out all 'at' users
    There was a bug in the at::user code that was commenting out any user
    that was added to the file.
    
    SIMP-3111 #close

--- spec/acceptance/suites/default/at_spec.rb
+++ spec/acceptance/suites/default/at_spec.rb
@@ -3,21 +3,6 @@ require 'spec_helper_acceptance'
 test_name 'at'
 
 describe 'at class' do
-  before(:context) do
-    hosts.each do |host|
-      interfaces = fact_on(host, 'interfaces').strip.split(',')
-      interfaces.delete_if do |x|
-        x =~ /^lo/
-      end
-
-      interfaces.each do |iface|
-        if fact_on(host, "ipaddress_#{iface}").strip.empty?
-          on(host, "ifup #{iface}", :accept_all_exit_codes => true)
-        end
-      end
-    end
-  end
-
   let(:manifest) {
     <<-EOS
       include 'at'
@@ -34,6 +19,5 @@ describe 'at class' do
         apply_manifest_on(host, manifest, :catch_changes => true)
       end
     end
-
   end
 end
